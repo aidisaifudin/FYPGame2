a048b44039891b7ed03a404411a137af06125aac
WanWeiJie <weijie2288@gmail.com>
2021-11-25 17:00:12 +0800

    Idk

--- FYPYear3Game2/Assets/Scripts/PassengerSpawner.cs
+++ FYPYear3Game2/Assets/Scripts/PassengerSpawner.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PassengerSpawner : MonoBehaviour
 {
-    public Transform passenger;
+    public GameObject passenger;
+    private float spawnRadius = 100.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 1; i < 3; i++)
         {
-            var position = new Vector3(Random.Range(-100.0f, 100.0f), 0, Random.Range(-100.0f, 100.0f));
-            Instantiate(passenger, position, Quaternion.identity);
+            Vector3 randomPos = Random.insideUnitCircle * spawnRadius;
+            Instantiate(passenger, randomPos, Quaternion.identity);
         }
     }
 
