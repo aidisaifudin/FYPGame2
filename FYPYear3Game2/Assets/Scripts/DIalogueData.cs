8e82997d70d5037a4a660dcd9ee46cde44fa3378
Toastyship <64509653+Toastyship@users.noreply.github.com>
2021-11-25 13:42:14 +0800

    test
    test

---
+++ FYPYear3Game2/Assets/Scripts/DIalogueData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New NPC", menuName ="NPC/New NPC")]
+public class DIalogueData : NPC
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
