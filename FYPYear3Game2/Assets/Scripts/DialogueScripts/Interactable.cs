cbf1163b0bf3914afa916f6f444130ca550265db
Toastyship <64509653+Toastyship@users.noreply.github.com>
2021-11-26 14:55:45 +0800

    Dialogue
    Dialogue

--- FYPYear3Game2/Assets/Scripts/DialogueScripts/Interactable.cs
+++ FYPYear3Game2/Assets/Scripts/DialogueScripts/Interactable.cs
@@ -1,18 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Interactable : MonoBehaviour
+public interface Interactable 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Interact( VehicleControl player);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
